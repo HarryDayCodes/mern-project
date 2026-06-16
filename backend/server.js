702ebb25975d7f1071d34bf0662e5fbd8c436c47
Harry Day <flurben@gmail.com>
2026-06-16 14:43:30 +0100

    Adding in initial routes handler

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,12 @@ const express = require('express') // Express package and all it's dependencies 
 // express app
 const app = express()
 
+// routes handler
+app.get('/', (req, rest) => {// Going to root "/", it'll fire the function..
+    res.json({mssg: 'Welcome to the app'})// function: returns a "response object" (res) which is a message
+})  
+
 // listen for requests
 app.listen(4000, () => {
     console.log('Listening on port 4000')
-})
+})  // To exit out of listening -> "ctrl + c"
