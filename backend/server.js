d7adfca5ffa73caea93655b10253ca7327d41b06
Harry Day <flurben@gmail.com>
2026-06-17 12:58:36 +0100

    Adding middleware + misc changes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
+// NOTE - To run - remember it's "npm run start" or 'node server.js" in terminal (the former just invokes the latter in package.json)
+require('dotenv').config()
+
 // This is basically a requirements package
 const express = require('express') // Express package and all it's dependencies go to node_modules
 
 // express app
 const app = express()
 
+// middleware (stuff between request on server and sending response)
+app.use((req, res, next) => {
+    console.log(req.path, req.method)  // logs the req method
+    next() // utilises next function so we move onto the next middleware no matter what
+})
+
 // routes handler
-app.get('/', (req, rest) => {// Going to root "/", it'll fire the function..
-    res.json({mssg: 'Welcome to the app'})// function: returns a "response object" (res) which is a message
+app.get('/', (req, res) => {// Sends a get request at root of localhost:4000
+    res.json({mssg: 'Welcome to the app'}) // This then sends a response message 
 })  
 
-// listen for requests
-app.listen(4000, () => {
-    console.log('Listening on port 4000')
+// listen for requests  
+app.listen(process.env.PORT, () => {  // PORT can be found in the .env variables
+    console.log('Listening on port ', process.env.PORT)
 })  // To exit out of listening -> "ctrl + c"
